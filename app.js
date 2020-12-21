c640d6b7930357d42be094357c4433477cf97760
Sergio Luis Beleño Díaz <38571812+Serbeld@users.noreply.github.com>
2020-12-20 21:08:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 var thumbs = new Swiper('.gallery-thumbs' , {
-
-    /** TODO */
-    
-      slidesPerView: 1,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      // Enable debugger
-      debugger: true
+     
+      spaceBetween: 10,
+      slidesPerView: 3,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
     });
+
+var main_thumb = new Swiper('.gallery-top', {
+      spaceBetween: 10,
+      loop: true,
+      navigation: {
+        nextEl: '.gallery-top .swiper-button-next',
+        prevEl: '.gallery-top .swiper-button-prev',
+      },
+      thumbs: {
+        swiper: thumbs
+      }
+    });
+
+$("button").click(
+    function(){
+        $("#la-calera-thumbs").addClass("popup")
+        $(".equis").show()
+    }
+)
+
+$(".equis").click(
+    function(){
+        $("#la-calera-thumbs").removeClass("popup")
+        $(".equis").hide()
+    }
+)
